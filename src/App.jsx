b062093e23ce4b3c3d920d2e590393d7e3d99b1b
Raihan Aditya <ra.raihanaditya@gmail.com>
2024-05-19 13:19:38 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -49,27 +49,27 @@ export default function App() {
     </div>
     </>
   )
+}
 
-  function calcualteWinner() {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ]
+function calcualteWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  for(let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines [i];
   
-    for(let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines [i];
-  
-      if (squares[a] && squares[a] === squares[b] && squares[c]) {
-        return squares[a];
-      }
+    if (squares[a] && squares[a] === squares[b] && squares[c]) {
+      return squares[a];
     }
-  
-    return false;
   }
+
+  return false;
 }
